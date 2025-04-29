0863377abd7df3e84933501546ccbcab3b8fccc9
hanna <tennyalana@yahoo.com>
2025-04-29 17:03:45 +0100

    loops

---
+++ loops.js
@@ -0,0 +1,11 @@
+//---loops----
+let region = ["us-east-1", "us-west-2", "eu-central-1"];
+for (let i = 0; i < region.length; i++) {
+  console.log(region[i]);
+}
+
+//---array of cloud service ---
+let cloudService = ["ec2", "s3", "lambda"];
+for (let i = 0; i < cloudService.length; i++) {
+  console.log(cloudService[i]);
+}
