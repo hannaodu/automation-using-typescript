186ce1777ca815e8f26f56486f146ca88d052534
hanna <tennyalana@yahoo.com>
2025-04-29 17:49:13 +0100

    while_loops

---
+++ while_loops.js
@@ -0,0 +1,17 @@
+
+//--while loops---
+
+let region = ["us-east-1", "us-west-2", "eu-central-1"];
+let i = 0;
+while (i < region.length) {
+  console.log(region[i]);
+  i++;
+}
+//---array of cloud service ---
+let cloudService = ["ec2", "s3", "lambda"];
+let j = 0;
+while (j < cloudService.length) {
+  console.log(cloudService[j]);
+  j++;
+}
+
