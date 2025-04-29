267853283ff07a971a733de4b6660290ac0ec522
hanna <tennyalana@yahoo.com>
2025-04-29 14:34:13 +0100

    data type comparison py and js

---
+++ datatype.js
@@ -0,0 +1,4 @@
+let x = 5;//number
+let y = 3.24;//number
+let name = 'hanna';//string
+let isValid = true;//boolen
---
+++ datatype.py
@@ -0,0 +1,4 @@
+x = 5 #int
+y = 3.24 #float
+name="hannah" #str
+is_valid = True #bool
